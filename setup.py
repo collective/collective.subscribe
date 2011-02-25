9cf8f495c2dbafd521af18f7d8e4ba45d1b6d79c
sean.upton@hsc.utah.edu <>
2011-02-25 15:42:49 -0700

    initial skeleton for uu.subscribe package

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1dev'
+
+setup(name='uu.subscribe',
+      version=version,
+      description="Components relating principals/users to content items.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: ZODB",
+        "Framework :: Zope3",
+        "Framework :: Plone",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        ],
+      keywords='',
+      author='Sean Upton',
+      author_email='sean.upton@hsc.utah.edu',
+      url='http://teamspace.upiq.org/trac',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['uu'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
