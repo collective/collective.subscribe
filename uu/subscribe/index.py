9595e18ec732ec2171c616fef6d5187c9cba3991
sean.upton@hsc.utah.edu <>
2011-03-06 23:20:10 -0700

    Added index and tests to uu.subscriber.

---
+++ uu/subscribe/index.py
@@ -0,0 +1,145 @@
+from persistent import Persistent
+from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
+from BTrees.OOBTree import OOBTree, OOSet
+
+from uu.subscribe.interfaces import ISubscriptionIndex, IItemSubscriber
+
+
+def _validate_signature(sig):
+    """validate signature is signature tuple, two items, both strings"""
+    if not isinstance(sig, tuple):
+        raise ValueError('subscriber signature must be tuple')
+    if len(sig) != 2:
+        raise ValueError('subscriber signature must be two items')
+    if not (isinstance(sig[0], str) and isinstance(sig[1], str)):
+        raise ValueError('subscriber signature elements must be strings')
+
+class ItemUIDToSignatureMapping(OOBTree):
+    """
+    OOBTree that validates keys as uid strings.
+    
+    Does not validate values.
+    """
+    
+    def __setitem__(self, key, value):
+        if not isinstance(key, basestring):
+            raise ValueError('Subscription index: key must be UID string')
+        elif isinstance(key, unicode):
+            key = key.encode('utf-8')
+        super(ItemUIDToSignatureMapping, self).__setitem__(key, value)
+
+
+class SignatureToItemUIDMapping(OOBTree):
+    """
+    OOBTree that validates keys as subscriber signature tuples. 
+
+    Does not validate values.
+    """
+    
+    def __setitem__(self, key, value):
+        _validate_signature(key)
+        super(SignatureToItemUIDMapping, self).__setitem__(key, value)
+
+
+class SubscriptionIndex(Persistent):
+    """
+    Subscription index maintains forward/reverse index mappings between
+    item UID strings and subscriber signature tuples.
+    """
+    implements(ISubscriptionIndex)
+    
+    name = FieldProperty(ISubscriptionIndex['name'])
+    
+    def __init__(self, name):
+        if isinstance(name, unicode):
+            name = name.encode('utf-8')
+        self.name = name
+        self._forward = ItemUIDToSignatureMapping()
+        self._reverse = SignatureToItemUIDMapping()
+
+    def _normalize_subscriber(self, sub):
+        """normalize subscriber or signature to signature"""
+        if IItemSubscriber.providedBy(sub):
+            sub = sub.signature()
+        _validate_signature(sub)
+        return sub
+    
+    def index(self, subscriber, item_uid):
+        """
+        Given an subscriber and and item_uid, associate for this index in
+        both (subscriber to items) and (item to subscribers) mappings.
+
+        The subscriber argument can be either a two-item tuple key or
+        an IItemSubsriber object, in which case the key will be extracted
+        by calling the signature() method of the subscriber.
+        """
+        # normalize key/value
+        signature = self._normalize_subscriber(subscriber)
+        item_uid = str(item_uid)
+        
+        # forward index
+        if item_uid not in self._forward:
+            self._forward[item_uid] = OOSet()
+        subscribers_for_item = self._forward[item_uid]
+        if signature not in subscribers_for_item:
+            subscribers_for_item.insert(signature)
+        
+        # reverse index
+        if signature not in self._reverse:
+            self._reverse[signature] = OOSet()
+        items_for_subscriber = self._reverse[signature]
+        if item_uid not in items_for_subscriber:
+            items_for_subscriber.insert(item_uid)
+        
+
+    
+    def unindex(self, subscriber, item_uid):
+        """
+        Given an subscriber and item_uid, remove any associations in this
+        index between them.
+        
+        The subscriber argument can be either a two-item tuple key or
+        an IItemSubsriber object, in which case the key will be extracted
+        by calling the signature() method of the subscriber.
+        """
+        # normalize key/value
+        signature = self._normalize_subscriber(subscriber)
+        item_uid = str(item_uid)
+        
+        # remove any association from forward index, if found
+        if item_uid in self._forward:
+            subscribers_for_item = self._forward[item_uid]
+            if signature in subscribers_for_item:
+                subscribers_for_item.remove(signature)
+        
+        # remove from reverse index, if found:
+        if signature in self._reverse:
+            items_for_subscriber = self._reverse[signature]
+            if item_uid in items_for_subscriber:
+                items_for_subscriber.remove(item_uid)
+            
+    def item_uids_for(self, subscriber):
+        """
+        Find, return tuple of item UIDs given a subscriber for this index.
+        
+        The subscriber argument can be either a two-item tuple key or
+        an IItemSubsriber object, in which case the key will be extracted
+        by calling the signature() method of the subscriber.        
+        """
+        signature = self._normalize_subscriber(subscriber)
+        if signature not in self._reverse:
+            return ()
+        return tuple(self._reverse[signature]) #iterate items/sub -> tuple
+    
+    def subscribers_for(self, item_uid):
+        """
+        Given an item UID, find and return a tuple of subscriber signatures
+        (composed keys) for subscribers an item in this index.
+        """
+        item_uid = str(item_uid)
+        if item_uid not in self._forward:
+            return ()
+        return tuple(self._forward[item_uid]) #iterate subs/item -> tuple
+
+
---
+++ uu/subscribe/tests/test_index.py
@@ -0,0 +1,106 @@
+import uuid
+import sys
+import unittest2 as unittest
+from new import instancemethod as methodtype
+
+import persistent
+from zope.interface import Invalid, implements
+from zope.schema import getSchemaValidationErrors, ValidationError
+
+from uu.subscribe.interfaces import IItemSubscriber, ISubscriptionIndex
+from uu.subscribe.subscriber import ItemSubscriber
+from uu.subscribe.index import SubscriptionIndex
+
+# MOCK COMMON:
+DATA = {
+            'name'  : u'Somebody',
+            'user'  : 'somebody',
+            'namespace' : 'member',
+            }
+
+DATAKEY = ('member', 'somebody')
+
+class MockSub(object):
+    implements(IItemSubscriber)
+    def __init__(self):
+        self.user = 'somebody'
+        self.namespace = 'member'
+        self.name = u'Somebody else'
+        self.email = None
+    def signature(self):
+        if self.email and self.namespace == 'email':
+            return ('email', self.email)
+        return (self.namespace, self.user or self.email)
+
+class IndexTest(unittest.TestCase):
+    """Test index of item uids to subscriber signatures"""
+
+    def _set_name(self, index, name):
+        index.name = name
+    
+    def test_name_field(self):
+        index = SubscriptionIndex('related')
+        assert index.name == 'related'
+        self._set_name(index, 'related again')
+        self.assertEqual(index.name, 'related again')
+        self.assertRaises(ValidationError, self._set_name, index, u'unicode')
+    
+    def test_index(self):
+        index = SubscriptionIndex('test_index')
+        uid = str(uuid.uuid4())
+        sub = MockSub()
+        assert not index.item_uids_for(sub)
+        assert not index.item_uids_for(sub.signature())
+        assert not index.subscribers_for(uid)
+
+        #try twice, will not duplicate index:
+        for i in range(0,2):
+            index.index(sub, uid)
+            result_uids = index.item_uids_for(sub)
+            result_subs = index.subscribers_for(uid)
+            assert len(result_uids) == 1
+            assert len(result_subs) == 1
+            assert uid in result_uids
+            assert sub.signature() in result_subs
+        
+        #index same intem, another sub:
+        sub2 = MockSub()
+        sub2.email = 'me@example.com'
+        sub2.namespace = 'email'
+        assert sub2.signature() == ('email', 'me@example.com')
+        index.index(sub2, uid)
+        result_uids = index.item_uids_for(sub2)
+        result_subs = index.subscribers_for(uid)
+        assert len(result_uids) == 1
+        assert len(result_subs) == 2
+        assert uid in result_uids
+        assert sub2.signature() in result_subs
+        assert sub.signature() in result_subs
+        
+        # return locals for continued use by other tests:
+        return locals()
+    
+    def test_unindex(self):
+        idx_locals = self.test_index()
+        index = idx_locals['index']
+        uid = idx_locals['uid']
+        sub = MockSub()
+        index.unindex(sub, uid)        
+        result_uids = index.item_uids_for(sub)
+        result_subs = index.subscribers_for(uid)
+        assert len(result_uids) == 0
+        assert len(result_subs) == 1
+        assert uid not in result_uids
+        assert sub.signature() not in result_subs
+        sub2sig = ('email', 'me@example.com')
+        assert sub2sig in result_subs #sub2 from above
+        index.unindex( sub2sig, uid ) #unindex sub2
+        result_uids = index.item_uids_for(sub2sig)
+        result_subs = index.subscribers_for(uid)
+        assert len(result_uids) == 0
+        assert len(result_subs) == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
+
