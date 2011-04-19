7aabdce0a9ff63406e6cc8fef1387feb04d6d1b5
sean.upton@hsc.utah.edu <>
2011-04-19 13:55:08 -0600

    uu.subscribe README doctest wrapped with test suite for zope.testing test runner.

--- uu/subscribe/tests/test_readme.py
+++ uu/subscribe/tests/test_readme.py
@@ -2,3 +2,15 @@ import doctest
 
 doctest.testfile('../README.txt')
 
+
+import doctest
+import unittest2 as unittest
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([doctest.DocFileSuite('../README.txt')])
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
+
